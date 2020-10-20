25aaf259d901977c1266e724cb2869a8e43ea374
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-10-20 21:48:18 +0300

    added bootstrap

--- js/script.js
+++ js/script.js
@@ -37,3 +37,51 @@ getPizzaOrder.prototype.finalPrice = function () {
     };
     this.price =(this.sizePrice +this.crustPrice + this.toppingPrice) * this.pizzaQuantity;
 };
+getPizzaOrder.prototype.toBeDelivered = function () {
+    if (this.delivery === "deliver") {
+        this.price += 300;
+    }else if(this.delivery ==="collect") {
+        this.price +=0;
+    };
+};
+function resetFieldValues () {
+    pizzaSize = "";
+    pizzaType = "";
+    extraToppings = "";
+    crustType = "";
+    $("#pizza-quantity").val("");
+};
+//  start of my user logic
+ $(document).ready(function() {
+     var modal = $(".ordering-plate");
+     var placeOrder = $(".place-order");
+     var close = $(".close-thing")
+     placeOrder.click(function() {
+         modal.show();
+     });
+     close.click(function() {
+         modal.hide();
+     });
+    $(".what-you-want").submit(function(event) {
+        event.preventDefault();
+        var pizzaSize = $("#pizza-size").val();
+        
+        var pizzaType = $("#pizzatype").val();
+        
+        var crustType = $("#crusttype").val();
+        
+        var extraToppings = $("#toppings").val();
+        
+        var pizzaQuantity = parseInt($("#pizza-quantity").val());
+     
+        var delivery = $("#delivery").val();
+       
+        var newPizzaOrder = new getPizzaOrder(pizzaType,crustType,extraToppings,pizzaQuantity,delivery,pizzaSize);
+        newPizzaOrder.finalPrice();
+      
+        newPizzaOrder.toBeDelivered();
+        alert ("you have ordered " + pizzaQuantity + " " + pizzaSize + " " + pizzaType + " pizza(s) with a  " + crustType + " crust and  " + extraToppings + " topping. It will be " + delivery + " .");
+        alert ("The total cost is  " + newPizzaOrder.price + " /=");
+        resetFieldValues();
+    });
+});
