82e90660f85300f45cfe1efd40429157df3c5c46
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-10-20 20:11:01 +0300

    added function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert("Hello! I am an alert box!!");
+function getPizzaOrder(pizzaType,crustType,extraToppings,pizzaQuantity,delivery,pizzaSize) {
+    this.pizzaType = pizzaType;
+    this.crustType = crustType;
+    this.extraToppings = extraToppings;
+    this.pizzaQuantity = pizzaQuantity;
+    this.delivery = delivery;
+    this.pizzaSize = pizzaSize;
+    this.sizePrice=0;
+    this.crustPrice = 0;
+    this.toppingPrice = 0;
+    this.deliveryPrice = 0;
+    this.price = 0;
+};
